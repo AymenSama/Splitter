d804ab5a6a5c6931ed0dba360c5fbab2f58f40cb
Aymen Sellami <Aymen.Sellami@hhu.de>
2023-05-24 15:29:34 +0200

    Refactor builder
    Extract try catch block to isNumeric method

--- src/main/java/splitter/interaction/PersonBuilder.java
+++ src/main/java/splitter/interaction/PersonBuilder.java
@@ -6,18 +6,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PersonBuilder {
+    private static boolean isNumeric(String string) {
+        try {
+            Double.parseDouble(string);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
     public static List<Person> build(String[] strings) {
         List<Person> accumulator = new ArrayList<>();
         Person p = null;
         for (String string : strings) {
-            try {
-                Double.parseDouble(string);
-            } catch (NumberFormatException e) {
+            if (isNumeric(string)) {
+                p.addExpense(string);
+            } else {
                 p = new Person(string);
                 accumulator.add(p);
-                continue;
             }
-            p.addExpense(string);
         }
         return accumulator;
     }
