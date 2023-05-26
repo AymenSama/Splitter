e21c3edc8a020ca06fde7edbb23ac8b20147456e
Aymen Sellami <Aymen.Sellami@hhu.de>
2023-05-26 19:42:24 +0200

    Implement splitting expenses for 2 people

--- src/main/java/splitter/ExpenseSplitter.java
+++ src/main/java/splitter/ExpenseSplitter.java
@@ -1,12 +1,40 @@
 package splitter;
 
 import splitter.data.Person;
+import splitter.finances.Money;
 import splitter.finances.Transfer;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ExpenseSplitter {
     public static List<Transfer> getTransfers(List<Person> people) {
-        return List.of();
+        if (people.size() == 0 || people.size() == 1) {
+            return List.of();
+        }
+        List<Transfer> transfers = new ArrayList<>();
+        BigDecimal count = new BigDecimal(people.size());
+
+        BigDecimal average = people.stream()
+                .map(Person::getExpenseTotal)
+                .reduce(BigDecimal.ZERO, BigDecimal::add)
+                .divide(count);
+
+        Person sender = people.get(0);
+        Person receiver = people.get(1);
+
+        if (sender.getExpenseTotal().compareTo(receiver.getExpenseTotal()) > 0) {
+            Person buffer = sender;
+            sender = receiver;
+            receiver = buffer;
+        }
+
+        Money owedMoney = new Money(receiver.getExpenseTotal().subtract(average));
+
+        Transfer transfer = new Transfer(sender, receiver, owedMoney);
+        transfers.add(transfer);
+
+        return transfers;
     }
 }
--- src/test/java/splitter/ExpenseSplitterTest.java
+++ src/test/java/splitter/ExpenseSplitterTest.java
@@ -3,6 +3,7 @@ package splitter;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import splitter.data.Person;
+import splitter.finances.Money;
 import splitter.finances.Transfer;
 
 import java.util.List;
@@ -17,12 +18,21 @@ public class ExpenseSplitterTest {
         List<Transfer> transfers = ExpenseSplitter.getTransfers(people);
         assertThat(transfers).isEmpty();
     }
-
     @Test
     @DisplayName("No transfers when one person has made an expense")
     void testOnePerson() {
         List<Person> people = List.of(new Person("Axel", "90"));
         List<Transfer> transfers = ExpenseSplitter.getTransfers(people);
         assertThat(transfers).isEmpty();
+    }
+    @Test
+    @DisplayName("Expenses get split equally between two people")
+    void testTwoPeople() {
+        Person axel = new Person("Axel", "90");
+        Person martin = new Person("Martin", "65.7");
+        Money owed = new Money("12.15");
+        List<Person> people = List.of(martin, axel);
+        List<Transfer> transfers = ExpenseSplitter.getTransfers(people);
+        assertThat(transfers).containsExactlyInAnyOrder(new Transfer(martin, axel, owed));
     }
 }
