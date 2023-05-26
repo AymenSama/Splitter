f603f09bb60b3fbc859bf5fbf1cc2aa2b1af5f66
Aymen Sellami <Aymen.Sellami@hhu.de>
2023-05-26 16:00:26 +0200

    Refactor constructor

--- src/main/java/splitter/finances/Transfer.java
+++ src/main/java/splitter/finances/Transfer.java
@@ -3,12 +3,9 @@ package splitter.finances;
 import splitter.data.Person;
 
 public record Transfer(Person sender, Person receiver, Money value) {
-    public Transfer(Person sender, Person receiver, Money value) {
+    public Transfer {
         if (sender.equals(receiver)) {
             throw new UnsupportedOperationException("Sender and receiver can not be identical");
         }
-        this.sender = sender;
-        this.receiver = receiver;
-        this.value = value;
     }
 }
