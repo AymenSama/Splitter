27e2f2294e1a9425b4d85aa4bcfa434025a04d63
Aymen Sellami <Aymen.Sellami@hhu.de>
2023-05-28 17:56:27 +0200

    Change methods in PersonBuilder to instance methods
    The advantages to this include simplifying method signatures by using global variables and making the implementation more flexible.

--- src/main/java/splitter/interaction/PersonBuilder.java
+++ src/main/java/splitter/interaction/PersonBuilder.java
@@ -8,45 +8,46 @@ import java.util.List;
 import static splitter.interaction.NumericString.isNumeric;
 
 public class PersonBuilder {
-    public static List<Person> build(String[] strings) {
+    private final List<Person> people = new ArrayList<>();
+
+    public List<Person> build(String[] strings) {
         if (strings == null) {
             throw new IllegalArgumentException("String array can't be null");
         }
 
-        List<Person> people = new ArrayList<>();
         Person person = null;
 
         for (String string : strings) {
             if (string == null) {
                 continue;
             }
-            person = handleString(string, person, people);
+            person = handleString(string, person);
             if (person != null) {
-                addIfAbsent(people, person);
+                addIfAbsent(person);
             }
         }
 
         return people;
     }
 
-    private static Person handleString(String string, Person person, List<Person> people) {
+    private Person handleString(String string, Person person) {
         if (isNumeric(string) && person != null) {
             person.addExpense(string);
             person = null;
         } else if (!isNumeric(string)) {
-            person = findOrMake(people, string);
+            person = findOrMake(string);
         }
         return person;
     }
 
-    private static Person findOrMake(List<Person> people, String name) {
+    private Person findOrMake(String name) {
         return people.stream()
                 .filter(p -> p.getName().equals(name))
                 .findAny()
                 .orElse(new Person(name));
     }
 
-    private static void addIfAbsent(List<Person> people, Person person) {
+    private void addIfAbsent(Person person) {
         if (!people.contains(person)) {
             people.add(person);
         }
--- src/test/java/splitter/interaction/PersonBuilderTest.java
+++ src/test/java/splitter/interaction/PersonBuilderTest.java
@@ -15,55 +15,55 @@ public class PersonBuilderTest {
     @DisplayName("An empty array has no people in it")
     void testNoPeople() {
         String[] input = {};
-        List<Person> people = PersonBuilder.build(input);
+        List<Person> people = new PersonBuilder().build(input);
         assertThat(people).isEmpty();
     }
     @Test
     @DisplayName("An array with one name generates a list with one person")
     void testOnePerson_noExpense() {
         String[] input = {"Martin"};
-        List<Person> people = PersonBuilder.build(input);
+        List<Person> people = new PersonBuilder().build(input);
         assertThat(people).containsExactlyInAnyOrder(new Person("Martin"));
     }
     @Test
     @DisplayName("An array with a name and a number builds a list with one person having that expense")
     void testOnePerson_expense() {
         String[] input = {"Bob", "25.89"};
-        List<Person> people = PersonBuilder.build(input);
+        List<Person> people = new PersonBuilder().build(input);
         assertThat(people).containsExactlyInAnyOrder(new Person("Bob", "25.89"));
     }
     @Test
     @DisplayName("An array with two names and two numbers builds a list with 2 people")
     void testTwoPeople() {
         String[] input = {"Bob", "25.89", "Tim", "80.9"};
-        List<Person> people = PersonBuilder.build(input);
+        List<Person> people = new PersonBuilder().build(input);
         assertThat(people).containsExactlyInAnyOrder(new Person("Bob", "25.89"), new Person("Tim", "80.9"));
     }
     @Test
     @DisplayName("Expenses get skipped when no names precede them")
     void testMissingNames() {
         String[] input = {"70" , "Bob", "25.89", "80", "102", "Tim"};
-        List<Person> people = PersonBuilder.build(input);
+        List<Person> people = new PersonBuilder().build(input);
         assertThat(people).containsExactlyInAnyOrder(new Person("Bob", "25.89"), new Person("Tim", "0"));
     }
     @Test
     @DisplayName("Passing null throws an IllegalArgumentException")
     void testNullArgument() {
-        Executable e = () -> PersonBuilder.build(null);
+        Executable e = () -> new PersonBuilder().build(null);
         assertThrows(IllegalArgumentException.class, e);
     }
     @Test
     @DisplayName("Null strings get skipped")
     void testNullStrings() {
         String[] input = {null ,"Bob", null, "Martin", "Tim"};
-        List<Person> people = PersonBuilder.build(input);
+        List<Person> people = new PersonBuilder().build(input);
         assertThat(people).containsExactlyInAnyOrder(new Person("Bob"), new Person("Martin"), new Person("Tim"));
     }
     @Test
     @DisplayName("Expenses get added to the same person when a name appears more than once in the array")
     void testNameDuplicate() {
         String[] input = {"Willy", "320", "Tim", "40", "Gaby", "48", "Tim", "100"};
-        List<Person> people = PersonBuilder.build(input);
+        List<Person> people = new PersonBuilder().build(input);
         assertThat(people).containsExactlyInAnyOrder(
                 new Person("Tim", "140"),
                 new Person("Willy", "320"),
